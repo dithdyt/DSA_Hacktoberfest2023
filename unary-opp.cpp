177fffd593d4b46ca107993aae57d1a61d878120
Ayuby <68719241+dithdyt@users.noreply.github.com>
2023-10-24 16:50:45 +0700

    Create unary-opp.cpp

---
+++ unary-opp.cpp
@@ -0,0 +1,18 @@
+// C++ program to demonstrate the use of 'unary minus'
+// operator
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int positiveInteger = 100;
+	int negativeInteger = -positiveInteger;
+
+	cout << "Positive Integer: " << positiveInteger << endl;
+	cout << "Negative Integer: " << negativeInteger << endl;
+
+	return 0;
+}
+
+// This code is contributed by sarajadhav12052009
